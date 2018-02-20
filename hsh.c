8856429c11672ca4f0f8e218a1310d27c57448e0
John Hawthorn <john.hawthorn@gmail.com>
2018-02-20 01:02:52 -0800

    Detect overflow of argv

--- hsh.c
+++ hsh.c
@@ -10,6 +10,10 @@
 
 #define CMD_MAX_LENGTH 4096
 
+// Max length of arguments including command itself and a NULL terminator.
+#define CMD_MAX_ARGS 32
+
+#define SHELL_NAME "hsh"
 #define PROMPT "\n%s\n$ "
 
 #include "commands.h"
@@ -21,13 +25,18 @@
 void hsh_exec(char *buf) {
 	int i;
 	int argc = 0;
-	char *argv[32];
+	char *argv[CMD_MAX_ARGS];
 	char *pch = strtok(buf, " \t\n");
-	while (pch != NULL) {
+	while (pch != NULL && argc < CMD_MAX_ARGS) {
 		argv[argc++] = strdup(pch);
 		pch = strtok(NULL, " \t\n");
 	}
+	if (argc >= CMD_MAX_ARGS) {
+		fprintf(stderr, SHELL_NAME ": too many arguments\n");
+		return;
+	}
 	argv[argc] = NULL;
+
 	if (argc) {
 		for (i = 0; execs[i].name; i++) {
 			if (!strcmp(execs[i].name, argv[0])) {
@@ -40,7 +49,7 @@ void hsh_exec(char *buf) {
 			waitpid(pid, NULL, 0);
 		} else {
 			execvp(argv[0], argv);
-			printf("error executing %s\n", argv[0]);
+			printf(SHELL_NAME "error executing %s\n", argv[0]);
 			exit(1);
 		}
 	}
@@ -60,9 +69,10 @@ void run() {
 		if (!line) {
 			printf("\n");
 			return;
+		} else if (*line && line[strlen(line) - 1] != '\n') {
+		} else {
+			hsh_exec(buf);
 		}
-
-		hsh_exec(buf);
 	}
 }
 
